24763afa00d28568defc4672be5c05fe5bcdb8c0
Jan Musioł <migosh@o2.pl>
2024-02-05 19:34:15 +0100

    Some Changes in submodule

--- Submodules_Main/Submodules_Main/Program.cs
+++ Submodules_Main/Submodules_Main/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Submodules_Main
+﻿using Submodules_external;
+
+namespace Submodules_Main
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Class1 t = new() { MyProperty = 1 };
+            Console.WriteLine(t.MyProperty);
         }
     }
 }
